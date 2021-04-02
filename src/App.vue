b89b9f97f40134722e1eb50baa7e04f81a8a8e7a
Иван Викторович <help.venanen@yandex.ru>
2021-04-02 15:51:47 +0300

    First Test Commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
     <v-app>
 
         <v-main>
+            <v-card
+                    class="mx-auto"
+                    max-width="400"
+                    tile
+            >
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>Button</v-list-item-title>
+                        <v-btn color="primary" fab="fab">Add</v-btn>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card>
             <component v-for="(item, i) in components" v-on="item.vOn" :is='item.components' v-bind="{...item.bind}"
                        :key="i" v-html="item.bind.content"
             ></component>
@@ -23,30 +35,44 @@ export default {
     },
 
     data: () => ({
+        addAvaible: [
+            {
+                'title': 'v-btn',
+            },
+            {
+                'title': 'v-btn',
+            },
+            {
+                'title': 'v-btn',
+            },
+            {
+                'title': 'v-btn',
+            },
+        ],
         components: [{
             component: 'v-btn',
             bind: {
                 is: 'v-btn',
                 color: 'primary',
                 content: 'test',
-                fab: 'true'
+                fab: true
             },
             vOn: {
                 click: () => alert('One')
             }
         },
             {
-            component: 'v-btn',
-            bind: {
-                is: 'v-btn',
-                color: 'dark',
-                content: 'test2',
-                fab: false
+                component: 'v-btn',
+                bind: {
+                    is: 'v-btn',
+                    color: 'dark',
+                    content: 'test2',
+                    fab: false
+                },
+                vOn: {
+                    click: () => alert('Two')
+                }
             },
-            vOn: {
-                click: () => alert('Two')
-            }
-        },
         ]
     }),
     mounted() {
