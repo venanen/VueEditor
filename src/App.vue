d40bb622bbcf45b06b6702cd5107ce8f4b5a01ab
Иван Викторович <help.venanen@yandex.ru>
2021-04-02 15:35:52 +0300

    First Test Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+
+        <v-main>
+            <component v-for="(item, i) in components" v-on="item.vOn" :is='item.components' v-bind="{...item.bind}"
+                       :key="i" v-html="item.bind.content"
+            ></component>
+
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// eslint-disable-next-line no-unused-vars
+import {VBtn} from 'vuetify/lib'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    components: {
+        VBtn
+    },
+
+    data: () => ({
+        components: [{
+            component: 'v-btn',
+            bind: {
+                is: 'v-btn',
+                color: 'primary',
+                content: 'test',
+                fab: 'true'
+            },
+            vOn: {
+                click: () => alert('One')
+            }
+        },
+            {
+            component: 'v-btn',
+            bind: {
+                is: 'v-btn',
+                color: 'dark',
+                content: 'test2',
+                fab: false
+            },
+            vOn: {
+                click: () => alert('Two')
+            }
+        },
+        ]
+    }),
+    mounted() {
+
+    }
+};
+</script>
